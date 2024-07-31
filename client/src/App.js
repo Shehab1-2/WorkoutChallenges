4423a36b0b347398ac80c7835af878b012b3b6c3
shehab1-2 <saa267@scarletmail.rutgers.edu>
2024-07-31 18:04:03 -0400

    connecting backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,32 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import ChallengeList from './components/ChallengeList'
 import axios from 'axios';
+import AddChallenge from './components/AddChallenge';
 function App() {
   
     const [challenges, setChallenges] = useState([]);
     useEffect(() =>{
-      const fetchChallenge = async () => {
+      fetchChallenge();
+    }, []);
+
+    const fetchChallenge = async () => {
+      try {
         const response = await axios.get('http://localhost:8080/challenges')
         setChallenges(response.data)
-      };
-      fetchChallenge();
+      } catch (error) {
+        console.error("Error fetching challenge ", error);
+      }
+      
+    };
 
-    }, [])
+    const handleChallengeAdded = () => {
+      fetchChallenge();
+    };
+
  return (
     <div className="App">
       <h1>Monthly Challenege</h1>
+      <AddChallenge onChallengeAdded={handleChallengeAdded}/>
       <ChallengeList challenges={challenges}/>
     </div>
   );
